0d769b5fd68a180ad915aa7f50244991bd26e82f
Ankit Singh <33923041+iamank1t@users.noreply.github.com>
2019-08-13 11:04:27 +0530

    MoviMania project created

---
+++ MovieMania/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieMania
+//
+//  Created by Ankit Singh on 12/08/19.
+//  Copyright © 2019 Ankit Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
